7f6df8b0041ba77211352903c80c95599049aecb
Saranya Sankaranarayanan <2827393s@student.gla.ac.uk>
2023-03-15 12:07:38 +0000

    Code optimization done

--- src/PTTApplication.java
+++ src/PTTApplication.java
@@ -1,14 +1,15 @@
-import datamodels.Administrator;
 import datamodels.CourseRequirement;
 import datamodels.Skill;
 import datamodels.Teacher;
 import datamodels.Training;
-import utils.CVSReader;
+import utils.CSVReader;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+
 
 public class PTTApplication {
 
@@ -26,14 +27,14 @@ public class PTTApplication {
 
 		// Populate skills list from CSV file
 		String path=new File("src/assets/Skills.csv").getAbsolutePath();		
-		ArrayList<Skill> skills = CVSReader.populateSkills(path);
+		ArrayList<Skill> skills = CSVReader.populateSkills(path);
 		System.out.println(skills);
 
 		System.out.println("\n******************************** COURSE REQUIREMENTS *********************************************");
 
 		// Populate courserequirements list from CSV file
 		path=new File("src/assets/CourseReq.csv").getAbsolutePath();
-		ArrayList<CourseRequirement> courseRequirements = CVSReader.populateCourseRequirements(path,skills);
+		ArrayList<CourseRequirement> courseRequirements = CSVReader.populateCourseRequirements(path,skills);
 		System.out.println(courseRequirements);
 		
 		
@@ -41,13 +42,13 @@ public class PTTApplication {
 
 		// Populate teachers list from CSV file
 		path = new File("src/assets/Teacher.csv").getAbsolutePath();
-		ArrayList<Teacher> teachers = CVSReader.populateTeachers(path,skills);
+		ArrayList<Teacher> teachers = CSVReader.populateTeachers(path,skills);
 		System.out.println(teachers);
 		
 		// Populate training list from CSV file
 		System.out.println("\n********************************** TRAINING **********************************************");
 		path=new File("src/assets/Trainings.csv").getAbsolutePath();
-		ArrayList<Training> trainings = CVSReader.populateTrainings(path,skills,teachers);
+		ArrayList<Training> trainings = CSVReader.populateTrainings(path,skills,teachers);
 		System.out.println(trainings);
 		
 		
@@ -65,7 +66,7 @@ public class PTTApplication {
 		
 		admin1.analyzeTerm(courseID,courseRequirements,teachers,trainings);
 		
-		//skills.get(0).setSkillName("test####");
+		//skills.get(0).setSkillName("test####"); // Updating skill name will reflect changes throughout all arraylists having skill object
 		
 		//System.out.println("\n********************************** UPDATED COURSE REQUIREMENTS **********************************************");		
 		//System.out.println(courseRequirements);
@@ -74,6 +75,7 @@ public class PTTApplication {
 		//System.out.println(trainings);
 
 		// Write to file
+		
 
 	}
 }
--- src/datamodels/Teacher.java
+++ src/datamodels/Teacher.java
@@ -1,6 +1,7 @@
 package datamodels;
 
 /** Datamodel to accomodate 'Teacher' object
+
  * 
  * @authors Praharsh and Mona
  *
@@ -10,7 +11,7 @@ package datamodels;
 import java.util.ArrayList;
 import java.util.List;
 
-import utils.CVSReader;
+import utils.CSVReader;
 
 
 /**Interacts with: Administrator Stores Teacher objects after reading a file as input 
@@ -53,38 +54,7 @@ public class Teacher extends Staff {
 		this.skills = teacherSkill;
 	}
 
-	// Method calls the filereader method in CSVReader
-	// and creates Teacher objects from the csv file
-	// It is called in main and given the file to read from
-
-//	public static ArrayList<Teacher> teachers(String filename) {
-//		ArrayList<Teacher> teachersList = new ArrayList<>();
-//		List<String[]> file = CVSReader.wholeFile(filename);
-//		// //why is this ID here?
-//		// int id = 1;
-//
-//		// Goes through the csv row by row (item)
-//		// first thing in item is the name, then id, then subject
-//		// Anything remaining on or after item[3] is a skill
-//		// numbered skills on or after item[3] will be added to the skills arrayList
-//		// Need to do it this way because there is a variable number of skills per
-//		// teacher
-//
-//		for (String[] item : file) {
-//			String name = item[1];// name
-//			int id = Integer.parseInt(item[0]);// teacher id
-//			String subject = item[2];// subject
-//			List<Integer> teacherSkill = new ArrayList<Integer>();
-//			for (int i = 3; i < item.length; i++) {// rest of the values are the skills
-//				teacherSkill.add(Integer.parseInt(item[i]));
-//			}
-//			Teacher teacher = new Teacher(id, name, subject);// initialize the object
-//			teacher.setTeacherSkills(teacherSkill);// set the skills
-//			teachersList.add(teacher);// add the teacher into the a list of teacher
-//		}
-//		return teachersList;
-//	}
-
+	
 
 	// print the skills
 	private String printSkills(ArrayList<Skill> s) {
